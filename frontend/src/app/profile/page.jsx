7ca072ab13a1b179025cf104581d25cb5ad96c53
preethicse109 <preethis19102004@gmail.com>
2026-01-28 13:02:41 +0530

    profile

--- frontend/src/app/profile/page.jsx
+++ frontend/src/app/profile/page.jsx
@@ -150,7 +150,7 @@ export default function ProfilePage() {
                                                     Site Photos
                                                 </a>
                                             </div>
-                                        </div>
+                                        </div>     
                                     ))}
                                 </div>
                             ) : (
